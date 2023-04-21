334ff7b75da03c78253d5e6d35a6c99e0f52981b
Noid03 <melladonicolas74@gmail.com>
2023-04-21 11:15:38 -0400

    reto2

--- Retos/reto2.py
+++ Retos/reto2.py
@@ -3,4 +3,18 @@
 #específico del arreglo creado. Todo esto utilizando import array.
 
 import array
+import random
+Numero =random.randint(10,30)
+Arreglo = array.array('i', [random.randint(0, 100) for _ in range(Numero)])
+print(Arreglo)
 
+busqueda = int(input("Ingrese el elemento que desea buscar en el arreglo:\n"))
+verificar = False
+for elemento in Arreglo:
+    if elemento == busqueda:
+        verificar = True
+        break
+if verificar:
+    print("El elemento",busqueda,"se encuentra en el arreglo")
+else:
+    print("El elemento",busqueda,"no se encuentra en el arreglo")
